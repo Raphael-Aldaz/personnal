968a977b481c339dd4f60aa319a6d5bac4cacc47
Raphael Aldaz <raphael.aldaz@gmail.com>
2024-09-19 10:43:32 +0200

    stylizing navbar

--- src/app/components/Main/app.component.ts
+++ src/app/components/Main/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from "../navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'RaphaelAldaz';
+  toggleMenu(){
+    const nav = document.querySelector('.nav');
+    const burger = document.querySelector('.menu__hamburger');
+    burger?.classList.toggle('open');
+    nav?.classList.toggle('open');
+  }
 }
